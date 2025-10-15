2bc3e31314a0696267c7f5158e5a14d09d1dca00
fuyyyyy <446889801@qq.com>
2025-10-15 14:43:32 +0800

    update

--- ttrain/ours1.sh
+++ ttrain/ours1.sh
@@ -7,6 +7,6 @@ export https_proxy="http://10.224.125.58:8022"
 
 # bash /vlm-ssd/fangyiyang/easyr1_emo/examples/fyy/qwen2_5_vl_3b_emo_prob_grpo.sh
 
-# bash /vlm-ssd/fangyiyang/easyr1_emo/examples/fyy/qwen2_5_vl_3b_naive_grpo.sh
+bash /vlm-ssd/fangyiyang/easyr1_emo/examples/fyy/qwen2_5_vl_3b_naive_grpo.sh
 
-bash /vlm-ssd/fangyiyang/easyr1_emo/examples/fyy/qwen2_5_vl_3b_emo_2stage_grpo.sh
+# bash /vlm-ssd/fangyiyang/easyr1_emo/examples/fyy/qwen2_5_vl_3b_emo_2stage_grpo.sh
